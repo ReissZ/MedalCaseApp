f9cf71b6da3935720e307f6347aba3e7f5fb67aa
reisszurbyk <reisszurbyk@live.ca>
2024-11-22 10:11:50 -0500

    Initial Commit

---
+++ MedalCaseApp/MedalCaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedalCaseAppApp.swift
+//  MedalCaseApp
+//
+//  Created by Reiss Zurbyk on 2024-11-22.
+//
+
+import SwiftUI
+
+@main
+struct MedalCaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MedalCaseAppTests/MedalCaseAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MedalCaseAppTests.swift
+//  MedalCaseAppTests
+//
+//  Created by Reiss Zurbyk on 2024-11-22.
+//
+
+import Testing
+@testable import MedalCaseApp
+
+struct MedalCaseAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
